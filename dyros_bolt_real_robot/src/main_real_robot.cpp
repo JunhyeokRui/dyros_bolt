b82478b0cabd5ff893defd62341edcc606c7ddc4
rui_dyrosrui <threeman1@snu.ac.kr>
2024-03-12 14:41:59 +0900

    debugging

--- dyros_bolt_real_robot/src/main_real_robot.cpp
+++ dyros_bolt_real_robot/src/main_real_robot.cpp
@@ -18,11 +18,6 @@ int main(int argc, char **argv)
     
     
     DyrosBoltInitArgs init_args;
-
-    struct sched_param param, param2;
-    pthread_attr_t attr, attr2;
-    pthread_t thread1;
-    int ret;
     
     int max_jnum = 6;
     init_args.q_start_ = max_jnum;
@@ -37,65 +32,8 @@ int main(int argc, char **argv)
         printf("[CAN - ERRO] init failed\n");
         return -1;
     }
-    /* Initialize pthread attributes (default values) */
-    ret = pthread_attr_init(&attr);
-    if (ret)
-    {
-        printf("init pthread attributes failed\n");
-        return ret;
-    }
 
-    /* Set scheduler policy and priority of pthread */
-    ret = pthread_attr_setschedpolicy(&attr, SCHED_FIFO);
-    if (ret)
-    {
-        printf("pthread setschedpolicy failed\n");
-        return ret;
-    }
-    param.sched_priority = 47+50;
-    ret = pthread_attr_setschedparam(&attr, &param);
-    if (ret)
-    {
-        printf("pthread setschedparam failed\n");
-        return ret;
-    }
-
-    cpu_set_t cpuset;
-    CPU_ZERO(&cpuset);
-    CPU_SET(7, &cpuset);
-
-    ret = pthread_attr_setaffinity_np(&attr, sizeof(cpuset), &cpuset);
-    if (ret)
-    {
-        printf("pthread setaffinity failed\n");
-        return ret;
-    }
-
-    /* Use scheduling parameters of attr */
-    ret = pthread_attr_setinheritsched(&attr, PTHREAD_EXPLICIT_SCHED);
-    if (ret)
-    {
-        printf("pthread setinheritsched failed\n");
-        return ret;
-    }
-
-    ret = pthread_create(&thread1, &attr, ethercatThread1, &init_args);
-    if (ret)
-    {
-        printf("create pthread 1 failed\n");
-        return ret;
-    }
-
-
-    pthread_attr_destroy(&attr);
-
-
-    /* Join the thread and wait until it is done */
-    ret = pthread_join(thread1, NULL);
-    if (ret)
-        printf("join pthread failed: %m\n");
-
-
+    ethercatThread1(&init_args);
     std::cout << "test5" << std::endl;
 
     printf("[CAN - INFO] cleaning up\n");
